75c7cfcae2f0f29b1eb1e2c5094f57068bf72c94
Burgas <burgassme@gmail.com>
2024-11-22 04:26:30 +0700

    Initial commit

---
+++ community-service/src/test/java/org/burgas/communityservice/CommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.burgas.communityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
