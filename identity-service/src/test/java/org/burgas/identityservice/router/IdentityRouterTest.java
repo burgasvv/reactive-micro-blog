1c436f36d5ff7c13fe93a626f7638ee74f42f1c4
Burgas <burgassme@gmail.com>
2024-11-29 19:20:46 +0700

    Initial commit

--- identity-service/src/test/java/org/burgas/identityservice/router/IdentityRouterTest.java
+++ identity-service/src/test/java/org/burgas/identityservice/router/IdentityRouterTest.java
@@ -1,6 +1,5 @@
 package org.burgas.identityservice.router;
 
-import org.burgas.identityservice.dto.IdentityRequestCreate;
 import org.burgas.identityservice.dto.IdentityResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
 
 
 @AutoConfigureWebFlux
@@ -32,30 +30,6 @@ class IdentityRouterTest {
         webTestClient.get()
                 .uri("/identities/admin")
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(IdentityResponse.class);
-    }
-
-    @Test
-    public void testCreateIdentity() {
-        webTestClient.post()
-                .uri("/identities/create")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(
-                        Mono.just(
-                                IdentityRequestCreate.builder()
-                                        .username("mouretter")
-                                        .firstname("Sapov")
-                                        .lastname("Alexander")
-                                        .patronymic("Sergeevich")
-                                        .email("mouretter@gmail.com")
-                                        .password("asdqwe")
-                                        .wallIsOpened(true)
-                                        .build()
-                        ),
-                        IdentityRequestCreate.class
-                )
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(IdentityResponse.class);
